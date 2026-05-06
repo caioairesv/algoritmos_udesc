fdf8a4d4bf458a0cc895b83de570e6820c012b38
Caio Aires <caioviniaires@gmail.com>
2026-05-06 14:46:42 -0300

    Add
    Primeira aula de C

---
+++ Lista13/Aula1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    float y;
+    char z;
+    char k[10];
+    printf("Digite um numero: ");
+    scanf("%i", &x);
+
+    if (x == 0 ) {
+        printf("O numero é zero");
+    }else {
+        printf("Não é zero");
+    }
+
+}
