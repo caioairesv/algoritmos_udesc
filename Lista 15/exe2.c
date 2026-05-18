f9ad42c9038c3cee1852c51edcaa00ca6e4deae6
Caio Aires <caioviniaires@gmail.com>
2026-05-18 14:05:28 -0300

    Add
    Refatorando código.

--- Lista 15/exe2.c
+++ Lista 15/exe2.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
-int main () {
 
-    double numerador = 4.0,soma = 0.0, somaB= 3.0;
-    int n, sinal = 1, pi = 3;
+int main() {
+
+    double numerador = 4.0, soma = 0.0, somaB = 3.0;
+    int n, sinal = 1;
 
     printf("Digite a quantidade de termos para as séries: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
     // Série de Gregory-Leibniz
-    for (int i = 1, j = 0; j < n; i+=2, j++) {
-        soma += sinal * numerador/i;
+    for (int i = 1, j = 0; j < n; i += 2, j++) {
+        soma += sinal * (numerador / i);
         sinal *= -1;
     }
+
     sinal = 1;
+
     // Série de Nilakantha
-    for (int i = 0, j =2, k = 3, l = 4; i < n; i++, j +=1, k += 1, l += 1) {
-        somaB += sinal * (numerador/(j*k*l));
-         sinal *= -1;
+    for (int i = 2, j = 0; j < n; i += 2, j++) {
+        somaB += sinal * (numerador / (i * (i + 1) * (i + 2)));
+        sinal *= -1;
     }
 
-    printf("Série de Gregory-Leibniz: %lf \n",soma);
-    printf("Série de Nilakantha: %lf \n",somaB);
+    printf("Série de Gregory-Leibniz: %.15lf\n", soma);
+    printf("Série de Nilakantha: %.15lf\n", somaB);
+
     return 0;
 }
