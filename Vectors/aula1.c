483bf2b9d6879644d9d8b22bfac80a93623504ba
Caio Aires <caioviniaires@gmail.com>
2026-06-02 16:46:40 -0300

    Add
    Adicionando novas tarefas.

--- Vectors/aula1.c
+++ Vectors/aula1.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#define k 10
 
 int  main () {
-    int vec[10], soma=0, maior = 0;
+    int vec[k], soma=0, maior = 0;
     float media;
 
 
-  for (int i = 0; i < 10; i++) {
+
+
+  for (int i = 0; i < k; i++) {
+
+      printf("Insira o valor: ");
       scanf("%d", &vec[i]);
 
       if (i == 0) {
@@ -17,13 +22,13 @@ int  main () {
       soma += vec[i];
   }
 
-    media = soma / 10;
+    media = soma / k;
 
     printf("A soma dos valores eh:%d\n", soma);
     printf("O maior valor eh:%d\n", maior);
     printf("A média dos valores eh:%f\n", media);
     printf("Segue os valores:");
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < k; i++) {
         printf("\n%d", vec[i]);
     }
 
